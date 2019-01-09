d25a1d49a64abc51274e11e16e6e2ba61eeba2b8
Fabio Matt <fabio.meihua08@gmail.com>
2019-01-08 23:16:37 -0200

    Adicionei arquivos via upload

---
+++ testes/src/testes/Acoes.java
@@ -0,0 +1,16 @@
+package testes;
+
+public class Acoes {
+	
+	
+	public static void main(String[] args) {
+	
+		Animal animal = new Animal();
+		
+		animal.latir();
+		
+		
+	
+
+}
+	}
---
+++ testes/src/testes/Animal.java
@@ -0,0 +1,20 @@
+package testes;
+
+public class Animal {
+	
+	private String name;
+	
+	
+	public Animal() {
+		
+	};
+	
+	public void latir() {
+		
+		System.out.println("Au , au");
+		
+	}
+	
+	
+
+}
